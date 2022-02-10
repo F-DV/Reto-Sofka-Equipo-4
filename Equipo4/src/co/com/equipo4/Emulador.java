74ca89b95260cb42a5112bbe0e13802a6e2a9a63
Juan_Esteban_Cardona <juanes.cardona@hotmail.com>
2022-02-09 20:39:49 -0500

    Terminado con comentarios.

--- Equipo4/src/co/com/equipo4/Emulador.java
+++ Equipo4/src/co/com/equipo4/Emulador.java
@@ -15,50 +15,58 @@ import static java.lang.Integer.parseInt;
 
 public class Emulador {
     static long[] registros = new long[43];
+
     public static String emular(String[] subrutina) {
+        int contador = 0;
         if (subrutina.length >= 1 && subrutina.length <= 1024) {
             for (int i = 0; i < subrutina.length; i++) {
-                String[] token = subrutina[i].split("[\\s+,]");
-                switch (token[0]) {
+                contador += 1;
+                if (contador <= 50000) {
+                    String[] token = subrutina[i].split("[\\s+,]");
+                    switch (token[0]) {
 
-                    case "MOV":
-                        metodoMOV(token[1], token[2]);
-                        break;
-                    case "ADD":
-                        metodoADD(token[1], token[2]);
-                        break;
-                    case "DEC":
-                        metodoDEC(token[1]);
-                        break;
-                    case "INC":
-                        metodoINC(token[1]);
-                        break;
-                    case "INV":
-                        metodoINV(token[1]);
-                        break;
-                    case "JMP":
-                        i = metodoJMP(token[1]);
-                        break;
-                    case "JZ":
-                        i = metodoJZ(token[1], i);
-                        break;
-                    case "NOP":
-                        break;
-                    default:
-                        System.out.println("No se reconoce la instrucción.");
+                        case "MOV":
+                            metodoMOV(token[1], token[2]);
+                            break;
+                        case "ADD":
+                            metodoADD(token[1], token[2]);
+                            break;
+                        case "DEC":
+                            metodoDEC(token[1]);
+                            break;
+                        case "INC":
+                            metodoINC(token[1]);
+                            break;
+                        case "INV":
+                            metodoINV(token[1]);
+                            break;
+                        case "JMP":
+                            i = metodoJMP(token[1]);
+                            break;
+                        case "JZ":
+                            i = metodoJZ(token[1], i);
+                            break;
+                        /**El método NOP, no hace nada y le hace break al ciclo.
+                         * @   */
+                        case "NOP":
+                            break;
+                        default:
+                            System.out.println("No se reconoce la instrucción.");
+                    }
+                } else {
+                    System.out.println("Se excedió el número de instrucciones (50000).");
                 }
             }
         } else {
-            System.out.println("Cantidad de instrucciones incorrecta.");
+            System.out.println("Cantidad de instrucciones incorrecta, deben ser (1-1024).");
         }
-        return "El resultado del R42 es: "+registros[42]+".";
+        return "El resultado del R42 es: " + registros[42] + ".";
     }
 
 
-
-
     /**
-     *El método MOV copia el valor de Rxx en el registro Ryy a partir de param1 y param2.
+     * El método MOV copia el valor de Rxx en el registro Ryy a partir de param1 y param2.
+     *
      * @param param1 Parametro 1 : Rxx
      * @param param2 Parametro 2 : Ryy
      */
@@ -73,8 +81,9 @@ public class Emulador {
     }
 
     /**
-     *El método ADD calcula la suma de los parámetros Rxx y Ryy y luego aplica el módulo a dicho número.
-     *Resultantes, este valor es finalmente almacenando en Rxx.
+     * El método ADD calcula la suma de los parámetros Rxx y Ryy y luego aplica el módulo a dicho número.
+     * Resultantes, este valor es finalmente almacenando en Rxx.
+     *
      * @param param1 Parametro 1 : Rxx
      * @param param2 Parametro 2 : Ryy
      */
@@ -86,10 +95,11 @@ public class Emulador {
         registros[parseInt(param1.substring(1))] = ((valorRxx + valorRyy) % (multi2ADD));
     }
 
-    /**El método DEC disminuye el valor del parámetro Rxx en 1
-    *se genera desbordamiento y el resultado es 0
-    *
-* @param param1 Parametro 1 : Rxx
+    /**
+     * El método DEC disminuye el valor del parámetro Rxx en 1
+     * se genera desbordamiento y el resultado es 0
+     *
+     * @param param1 Parametro 1 : Rxx
      */
     public static void metodoDEC(String param1) {
         if (registros[parseInt(param1.substring(1))] == 0) {
@@ -101,7 +111,8 @@ public class Emulador {
     }
 
     /**
-     *El método INC aumenta el valor del parámetro Rxx en 1
+     * El método INC aumenta el valor del parámetro Rxx en 1
+     *
      * @param param1 Parametro 1 : Rxx
      */
     public static void metodoINC(String param1) {
@@ -115,6 +126,7 @@ public class Emulador {
 
     /**
      * Método INV invertirá el valor del parámetro Rxx.
+     *
      * @param param1 Parametro 1 : Rxx
      */
     public static void metodoINV(String param1) {
@@ -131,8 +143,10 @@ public class Emulador {
         String valorINVinv = sb.toString();
         registros[parseInt(param1.substring(1))] = Long.parseLong(valorINVinv, 2);
     }
-/**
+
+    /**
      * Método JMP saltará al parámetro indicado d, garantizando que esta instrucción es válida.
+     *
      * @param param1 Parametro 1 : Rxx
      */
     public static int metodoJMP(String param1) {
@@ -142,8 +156,9 @@ public class Emulador {
 
     /**
      * Metodo JZ saltará al parámetro si el valor del registro Rxx es 0
+     *
      * @param param1 Parametro 1 : Rxx
-     * @param index Indice de la instrucción
+     * @param index  Indice de la instrucción
      * @return Indice de la instrucción
      */
     public static int metodoJZ(String param1, int index) {
